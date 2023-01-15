aae0976b8fc4a7b0bd62b6f216481d21cc1bfbff
DMATH6 <dylanmatheson484@gmail.com>
2023-01-15 20:37:01 +0800

    Added new function that prints account data per line, re-did a few things
    .

--- ATM-ConsoleApp.cpp
+++ ATM-ConsoleApp.cpp
@@ -7,95 +7,109 @@
 #include <ctime> 
 #include <cstdlib> 
 #include <algorithm>
+using namespace std;
 
-int userdata()
+auto UserdataGenerator(std::vector <std::string> CustomerOutputInfo)
 {
-    std::vector <std::string> NameFirst;
-    std::vector <std::string> NameLast;
-    std::vector <float> CurrentBalence;
  
-    //SET USER DATA MANUALLY UNTIL WE SET UP CSV IMPORTS
+    std::vector <std::string> nameFirst;
+    std::vector <std::string> nameLast;
+    std::vector <float> currentBalence;
+    std::vector <std::string> currentBalenceString;
+    
+ 
 
-    //RANDOM LIST OF NAMES
-    std::vector <std::string> NameRando;
+    //list of names we use to generate account names
+    std::vector <std::string>nameRando;
 
-    NameRando.push_back("Jerry");
-    NameRando.push_back("Bob");
-    NameRando.push_back("Smith");
-    NameRando.push_back("Kyle");
-    NameRando.push_back("Mr Beast");
-    NameRando.push_back("Amongus");
-    NameRando.push_back("Terry");
-    NameRando.push_back("Brent");
-    NameRando.push_back("Katey");
-    NameRando.push_back("Bron");
-    NameRando.push_back("Kent");
-    NameRando.push_back("Klark");
-    NameRando.push_back("Ben");
-    NameRando.push_back("Smithy");
-    NameRando.push_back("Rob");
-    NameRando.push_back("Bobby");
-    NameRando.push_back("Hank");
-    NameRando.push_back("Stacy");
-    NameRando.push_back("Sharen");
-    NameRando.push_back("Shannon");
-    NameRando.push_back("BigBooty");
-    NameRando.push_back("Ellie");
-    NameRando.push_back("Ethan");
-    NameRando.push_back("Roberts");
+   nameRando.push_back("Jerry");
+   nameRando.push_back("Bob");
+   nameRando.push_back("Smith");
+   nameRando.push_back("Kyle");
+   nameRando.push_back("Mr Beast");
+   nameRando.push_back("Amongus");
+   nameRando.push_back("Terry");
+   nameRando.push_back("Brent");
+   nameRando.push_back("Katey");
+   nameRando.push_back("Bron");
+   nameRando.push_back("Kent");
+   nameRando.push_back("Klark");
+   nameRando.push_back("Ben");
+   nameRando.push_back("Smithy");
+   nameRando.push_back("Rob");
+   nameRando.push_back("Bobby");
+   nameRando.push_back("Hank");
+   nameRando.push_back("Stacy");
+   nameRando.push_back("Sharen");
+   nameRando.push_back("Shannon");
+   nameRando.push_back("BigBooty");
+   nameRando.push_back("Ellie");
+   nameRando.push_back("Ethan");
+   nameRando.push_back("Roberts");
 
-    //RANDOM LIST OF Bank Amounts
+    //used to seed so we can have random account balances 
     srand(time(NULL));  
     double random;
 
 
-    std::vector <float> AmountRando;
 
 
     
-    //Grabs random bank account values
+    //grabs random bank account values useing random
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
     for (int count = 0; count < 100; ++count)
     {
-        
-        CurrentBalence.push_back((rand() % (5000000+ 1 - 0))/100);
-        
+       currentBalence.push_back((rand() % (5000000+ 1 - 0))/100);
+       currentBalenceString.push_back(to_string(currentBalence.at(count)));
   
     }
   
-
+    
     
 
   
 
-    //Grabs the first and lastname at random
-    int FirstAndLastNameSize = NameRando.size();
+    //grabs the first and lastname at random 
+    int FirstAndLastNameSize =nameRando.size();
 
     for (int count = 0; count < FirstAndLastNameSize; ++count)
     {
-        NameFirst.push_back(NameRando.at(count));
-        NameLast.push_back(NameRando.at(count));
+        nameFirst.push_back(nameRando.at(count));
+        nameLast.push_back(nameRando.at(count));
         
 
     }
 
     srand(unsigned(time(NULL)));
-    std::random_shuffle(NameFirst.begin(), NameFirst.end());
+    std::random_shuffle(nameFirst.begin(), nameFirst.end());
 
+
+    //combines it all into a single string and push backs into CustomerOutputInfo
     for (int count = 0; count < FirstAndLastNameSize; ++count)
     {
-        std::cout << "\n" << NameFirst.at(count) << " " << NameLast.at(count) << " " << "$" << CurrentBalence.at(count);
-       
+        
+        CustomerOutputInfo.push_back(nameFirst.at(count).append(" ").append(nameLast.at(count)).append(" ").append("$").append(currentBalenceString.at(count)));
 
-
+      
     }
- 
-    return 0;
+    
+    return CustomerOutputInfo;
 
 }
 
+auto DisplayAccountInfoOneLine(std::vector <std::string> CustomerOutputInfo, int index)
+{
+    int indexOfArray;
+    std::cout << CustomerOutputInfo.at(index);
+}
 
 int main()
 {
-    userdata();
+    std::vector <std::string> CustomerOutputInfo;//what we pass from userdataGenerator to displayAccountInfo and other functions
+
+
+    UserdataGenerator(CustomerOutputInfo); //creates dummy data
+    DisplayAccountInfoOneLine(UserdataGenerator(CustomerOutputInfo), 1); //prints account info at specified index (account balances are strings)
+
+ 
 }
