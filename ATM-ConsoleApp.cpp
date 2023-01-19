f7a7bf766fc596bcf1196d272d03cfe1f5437663
DMATH6 <dylanmatheson484@gmail.com>
2023-01-19 08:45:08 +0800

    Re Wrote Code
    Re did the code to support structs since its easier to pass a struct for multiple output parameters (hey don't blame me i used Unreal blueprints which do this for me).

--- ATM-ConsoleApp.cpp
+++ ATM-ConsoleApp.cpp
@@ -10,160 +10,193 @@
 #include <Windows.h>
 #include <iomanip>
 #include <conio.h>
+#include <random>
 
-using namespace std;
+int numberOFRowsToGenerate = 250; //we use this to cap how many fake accounts we generate
 
-auto UserdataGenerator(std::vector <std::string> CustomerOutputInfo)
+
+struct accountData
 {
- 
-    std::vector <std::string> nameFirst;
-    std::vector <std::string> nameLast;
-    std::vector <float> currentBalence;
-    std::vector <std::string> currentBalenceString;
-    
- 
-
-    //list of names we use to generate account names
-    std::vector <std::string>nameRando;
-
-   nameRando.push_back("Jerry");
-   nameRando.push_back("Bob");
-   nameRando.push_back("Smith");
-   nameRando.push_back("Kyle");
-   nameRando.push_back("Mr Beast");
-   nameRando.push_back("Amongus");
-   nameRando.push_back("Terry");
-   nameRando.push_back("Brent");
-   nameRando.push_back("Katey");
-   nameRando.push_back("Bron");
-   nameRando.push_back("Kent");
-   nameRando.push_back("Klark");
-   nameRando.push_back("Ben");
-   nameRando.push_back("Smithy");
-   nameRando.push_back("Rob");
-   nameRando.push_back("Bobby");
-   nameRando.push_back("Hank");
-   nameRando.push_back("Stacy");
-   nameRando.push_back("Sharen");
-   nameRando.push_back("Shannon");
-   nameRando.push_back("BigBooty");
-   nameRando.push_back("Ellie");
-   nameRando.push_back("Ethan");
-   nameRando.push_back("Roberts");
-
-    //used to seed so we can have random account balances 
-    srand(time(NULL));  
-    double random;
+	std::string userFirstName;
+	std::string userLastName;
+	std::string gender;
+	std::string age;
+	std::string occupation;
+	std::string lastAccessed;
+	int phoneNumber;
+	float accountBalence;
+	int accountNum;
+};
 
 
 
-
-    
-    //grabs random bank account values useing random
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-    for (int count = 0; count < 100; ++count)
-    {
-       currentBalence.push_back((rand() % (5000000+ 1 - 0))/100);
-       currentBalenceString.push_back(to_string(currentBalence.at(count)));
-    }
-  
-    
-    
-
-  
-
-    //grabs the first and lastname at random 
-    int FirstAndLastNameSize =nameRando.size();
-
-    for (int count = 0; count < FirstAndLastNameSize; ++count)
-    {
-        nameFirst.push_back(nameRando.at(count));
-        nameLast.push_back(nameRando.at(count));
-        
-
-    }
-
-    srand(unsigned(time(NULL)));
-    std::random_shuffle(nameFirst.begin(), nameFirst.end());
-
-
-    //combines it all into a single string and push backs into CustomerOutputInfo
-    for (int count = 0; count < FirstAndLastNameSize; ++count)
-    {
-        
-        CustomerOutputInfo.push_back(nameFirst.at(count).append(" ").append(nameLast.at(count)).append("                                                       ").append("$").append(currentBalenceString.at(count)));
-
-      
-    }
-    
-    return CustomerOutputInfo;
-
-}
-
-auto DisplayAccountInfoOneLine(std::vector <std::string> CustomerOutputInfo, int index)
+auto GenerateUserData()
 {
-    int indexOfArray;
-    std::cout << (CustomerOutputInfo.at(index));
-}
+	//NAME LIST (FIRST NAMES)
+	std::vector<std::string> namesInListFirst;
+
+	namesInListFirst.push_back("Genny");
+	namesInListFirst.push_back("Bob");
+	namesInListFirst.push_back("Mr Beast");
+	namesInListFirst.push_back("Brent");
+	namesInListFirst.push_back("Bobby");
+	namesInListFirst.push_back("Sadie");
+	namesInListFirst.push_back("Roberts");
+	namesInListFirst.push_back("Dylan");
+	namesInListFirst.push_back("James");
+	namesInListFirst.push_back("Quin");
+	namesInListFirst.push_back("Rachel");
+	namesInListFirst.push_back("Amongus");
+	namesInListFirst.push_back("Lisa");
+	namesInListFirst.push_back("Homer");
+	namesInListFirst.push_back("Jenny");
+	namesInListFirst.push_back("John");
+	namesInListFirst.push_back("Hank");
+	namesInListFirst.push_back("Hill");
+	namesInListFirst.push_back("Samantha");
+	namesInListFirst.push_back("Andrew");
+	namesInListFirst.push_back("Waltah");
+
+	//NAME LIST (LAST NAMES)
+	std::vector<std::string> namesInListLast;
+
+	namesInListLast.push_back("Genny");
+	namesInListLast.push_back("Bob");
+	namesInListLast.push_back("Mr Beast");
+	namesInListLast.push_back("Brent");
+	namesInListLast.push_back("Bobby");
+	namesInListLast.push_back("Sadie");
+	namesInListLast.push_back("Roberts");
+	namesInListLast.push_back("Dylan");
+	namesInListLast.push_back("James");
+	namesInListLast.push_back("Quin");
+	namesInListLast.push_back("Iffle");
+	namesInListLast.push_back("Matheson");
+	namesInListLast.push_back("Lisa");
+	namesInListLast.push_back("Homer");
+	namesInListLast.push_back("Jenny");
+	namesInListLast.push_back("John");
+	namesInListLast.push_back("Hank");
+	namesInListLast.push_back("Hill");
+	namesInListLast.push_back("Samantha");
+	namesInListLast.push_back("Andrew");
+	namesInListLast.push_back("Waltah");
+	namesInListLast.push_back("White");
+
+	//GENDER LIST
+	std::vector<std::string> gendersInList;
+	gendersInList.push_back("Male");
+	gendersInList.push_back("Female");
+	gendersInList.push_back("Cisgender");
+	gendersInList.push_back("Transgender");
+	gendersInList.push_back("Cishet"); 
+	gendersInList.push_back("Non - binary");
+	gendersInList.push_back("Intersex");
+	gendersInList.push_back("Genderqueer");
+	gendersInList.push_back("Gender-fluid");
+	gendersInList.push_back("Gender Non-conforming");
+	gendersInList.push_back("Gender-Expansive");
+	gendersInList.push_back("Agender");
+	gendersInList.push_back("Gendervoid");
+	gendersInList.push_back("Bigender");
+	gendersInList.push_back("Omnigender");
+	gendersInList.push_back("Two-spirit");
+	gendersInList.push_back("Pangender");
+
+
+	//OCCUPATION LIST
+	std::vector<std::string> occupationsInList;
+	occupationsInList.push_back("Yard Hand");
+	occupationsInList.push_back("Retail Worker");
+	occupationsInList.push_back("Pizza Flipper");
+	occupationsInList.push_back("Fry Cook");
+	occupationsInList.push_back("CEO");
+	occupationsInList.push_back("I.T");
+	occupationsInList.push_back("Warehouse");
+	occupationsInList.push_back("Sexy Dancer");
+	occupationsInList.push_back("Talent Delivery Specialist");
+	occupationsInList.push_back("Snake Milker");
+	occupationsInList.push_back("Chick Sexer");
+	occupationsInList.push_back("Lab Rat");
+	occupationsInList.push_back("Media Distribution Officer");
+	occupationsInList.push_back("Animal Colourist");
+	occupationsInList.push_back("Hair Boiler");
+	occupationsInList.push_back("Digital Overlord");
+	occupationsInList.push_back("Reindeer Walker");
+	occupationsInList.push_back("Retail Jedi");
+	occupationsInList.push_back("Slaughterer");
+	occupationsInList.push_back("Wizard of Light Bulb Moments");
+	occupationsInList.push_back("Direct Mail Demi-God");
+	occupationsInList.push_back("Wet Leisure Attendant");
+	occupationsInList.push_back("Grand Master of Underlings");
+	occupationsInList.push_back("Twisted Brother");
+	occupationsInList.push_back("Under Secretary to the Sub-Committee");
+	occupationsInList.push_back("Ghost Hunter");
+	occupationsInList.push_back("Zombie");
+	occupationsInList.push_back("Teddy Bear Surgeon ");
+
+	//GENERATE PHONE NUMBERS BEGIN==================================================================================================
+	std::vector<int> PhoneNumbersInList;
+	srand((unsigned int)time(NULL));
+
+
+
+
+	
+	for (int count = 0; count <= numberOFRowsToGenerate; ++count)
+	{
+		PhoneNumbersInList.push_back(int(rand() % 49999999 + 49300000));
+	};
+
+	//GENERATE PHONE NUMBERS END====================================================================================================
+
+
+	std::vector <accountData> collectionUserData;
+
+
+	//SHUFFLE FIRST NAME AND LAST NAME BEGIN========================================================================================
+
+
+	auto rd = std::random_device{};
+	auto rng = std::default_random_engine{ rd() };
+
+	//SHUFFLE FIRST NAME AND LAST NAME END==========================================================================================
+
+
+	//ADD TO STRUCT BEGIN===========================================================================================================
+	for (int count = 0; count <= numberOFRowsToGenerate; ++count)
+	{
+
+		std::shuffle(namesInListFirst.begin(), namesInListFirst.end(), rng);
+		std::shuffle(namesInListLast.begin(), namesInListLast.end(), rng);
+		std::shuffle(PhoneNumbersInList.begin(), PhoneNumbersInList.end(), rng);
+
+	 accountData temp
+		{
+			namesInListFirst.at(0),
+			namesInListLast.at(0),
+			"",
+			"",
+			"",
+			"",
+			PhoneNumbersInList.at(0)
+
+		};
+	 collectionUserData.push_back(temp);
+
+	 std::cout << collectionUserData.at(count).userFirstName << "   " << collectionUserData.at(count).userLastName << "    " << collectionUserData.at(count).phoneNumber << std::endl;
+	};
+	//ADD TO STRUCT END=============================================================================================================
+
+	return 0;
+	
+
+
+};
+
 
 int main()
 {
-    static CONSOLE_FONT_INFOEX  fontex;
-    fontex.cbSize = sizeof(CONSOLE_FONT_INFOEX);
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    GetCurrentConsoleFontEx(hOut, 0, &fontex);
-    fontex.FontWeight = 12200;
-    fontex.dwFontSize.X = 36;
-    fontex.dwFontSize.Y = 36;
-    system("color 87");
-    SetCurrentConsoleFontEx(hOut, NULL, &fontex);
-
-
-
-    std::vector <std::string> CustomerOutputInfo;//what we pass from userdataGenerator to displayAccountInfo and other functions
-
-
-
-    cout << "Welcome To The Bank" << endl;
-    cout << "===================================================================================" << endl;
-    cout << "Press 1 To Login" << endl;
-    cout << "===================================================================================" << endl;
-
-
-
-
-    int x;
-    int accountNum;
-    cin >> x;
-
-    if (x == 1)
-    {
-        UserdataGenerator(CustomerOutputInfo); //creates dummy data
-        
-
-    }
-
-    else
-    {
-        cout << "Invalid Selection" << endl;
-    }
-
-    system("cls");
-
-
-    cout << "===================================================================================" << endl;
-    cout << "Please Enter Your Account Number" << endl;
-    cout << "===================================================================================" << endl;
-    
-    cin >> accountNum;
-    system("cls");
-    
-    cout << "NAME=============================================================AMOUNT============" << endl;
-    cout << std::setfill('=') << endl;
-    DisplayAccountInfoOneLine(UserdataGenerator(CustomerOutputInfo), accountNum); //prints account info at specified index (account balances are strings)
-    cout << "\n \n \n" << endl;
-
-    cout << "===================================================================================" << endl;
-    cout << "Press 2 To Withdraw" << endl;
-    cout << "===================================================================================" << endl;
+	
+	GenerateUserData();
 }
